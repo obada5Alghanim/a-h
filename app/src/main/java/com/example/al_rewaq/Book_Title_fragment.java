c46e6980b7575da729e882b957928c3d215a2278
user <abada.alghanim55@gmail.com>
2024-04-12 01:21:12 +0300

    make a see more and see less change the size of categories Layout and sign up  DOB

--- app/src/main/java/com/example/al_rewaq/Book_Title_fragment.java
+++ app/src/main/java/com/example/al_rewaq/Book_Title_fragment.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Book_Title_fragment extends Fragment {
 
@@ -15,7 +18,50 @@ public class Book_Title_fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_book__title_fragment, container, false);
+
+      TextView  textViewContent = view.findViewById(R.id.textViewContent);
+//        buttonSeeMore = findViewById(R.id.buttonSeeMore);
+       TextView textSeeLess = view.findViewById(R.id.textSee);
+        textSeeLess.setOnClickListener(new View.OnClickListener() {
+            int less = 0;
+
+            @Override
+            public void onClick(View v) {
+
+                // Expand the TextView
+//                textViewContent.setMaxLines(Integer.MAX_VALUE);
+//                textSeeLess.setVisibility(View.GONE);
+                if (less == 0) {
+                    textViewContent.setMaxLines(Integer.MAX_VALUE);
+//                textSeeLess.setVisibility(View.GONE);
+                    textSeeLess.setText("see less");
+                    less++;
+                } else if (less == 1) {
+                    textViewContent.setMaxLines(2);
+//                 textViewContent.setMinLines(Integer.MIN_VALUE);
+//                 textSeeLess.setVisibility(View.GONE);
+                    textSeeLess.setText("see more");
+                    less--;
+                }
+
+            }
+        });
+        textViewContent.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                textViewContent.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                // Detect when text is fully displayed
+                if (textViewContent.getLayout() != null && textViewContent.getLineCount() > 0) {
+                    if (textViewContent.getLayout().getEllipsisCount(textViewContent.getLineCount() - 1) > 0) {
+                        // Text is truncated, show "See More" button
+                        textSeeLess.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book__title_fragment, container, false);
+        return view;
     }
 }
--- app/src/main/java/com/example/al_rewaq/menu_main.java
+++ app/src/main/java/com/example/al_rewaq/menu_main.java
@@ -1,7 +1,13 @@
 package com.example.al_rewaq;
 
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -12,9 +18,11 @@ import com.google.android.material.navigation.NavigationBarView;
 
 public class menu_main extends AppCompatActivity {
 
-
+  //  private TextView textViewContent;
+//    private Button buttonSeeMore;
 BottomNavigationView bottomNavigationView;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,24 +33,25 @@ BottomNavigationView bottomNavigationView;
         home_page_fragment homePageFragment = new home_page_fragment();
         Categories_fragment categoriesFragment = new Categories_fragment();
         search_fragment searchFragment = new search_fragment();
+        Book_Title_fragment Book_Title_fragment = new Book_Title_fragment();
 
+        getSupportFragmentManager().beginTransaction().replace(android.R.id.content,homePageFragment).commit();
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected( MenuItem item) {
+                if(item.getItemId() == R.id.home_icon1){
+                    getSupportFragmentManager().beginTransaction().replace(android.R.id.content,homePageFragment).commit();
+                } else if (item.getItemId() == R.id.category_icon) {
+                    getSupportFragmentManager().beginTransaction().replace(android.R.id.content,categoriesFragment).commit();
+                } else if (item.getItemId() == R.id.search_icon) {
+                    getSupportFragmentManager().beginTransaction().add(android.R.id.content,searchFragment).commit();
+                } if (item.getItemId() == R.id.library_icon) {
+                    getSupportFragmentManager().beginTransaction().replace(android.R.id.content,Book_Title_fragment ).commit();
+                }
 
-
-   getSupportFragmentManager().beginTransaction().replace(android.R.id.content,homePageFragment).commit();
-    bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected( MenuItem item) {
-           if(item.getItemId() == R.id.home_icon1){
-               getSupportFragmentManager().beginTransaction().replace(android.R.id.content,homePageFragment).commit();
-           } else if (item.getItemId() == R.id.category_icon) {
-               getSupportFragmentManager().beginTransaction().replace(android.R.id.content,categoriesFragment).commit();
-           } else if (item.getItemId() == R.id.search_icon) {
-               getSupportFragmentManager().beginTransaction().add(android.R.id.content,searchFragment).commit();
-           }
-
-            return true;
-        }
-    });
+                return true;
+            }
+        });
 
 
 
