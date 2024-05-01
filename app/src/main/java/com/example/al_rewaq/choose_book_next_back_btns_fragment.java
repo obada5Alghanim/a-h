5fc289ed67e1c5daa318ce3a45365131644acef8
AsFoOr130 <mohalenezi130@gmail.com>
2024-05-01 23:16:36 +0300

    wdwdwdwdwdwd

---
+++ app/src/main/java/com/example/al_rewaq/choose_book_next_back_btns_fragment.java
@@ -0,0 +1,21 @@
+package com.example.al_rewaq;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class choose_book_next_back_btns_fragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_choose_book_next_back_btns_fragment, container, false);
+    }
+}
--- app/src/main/java/com/example/al_rewaq/nav_drawer_menu_fragment.java
+++ app/src/main/java/com/example/al_rewaq/nav_drawer_menu_fragment.java
@@ -1,6 +1,5 @@
 package com.example.al_rewaq;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -13,24 +12,22 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.google.firebase.auth.FirebaseAuth;
-
 import org.w3c.dom.Text;
 
 
 public class nav_drawer_menu_fragment extends Fragment {
 
-FirebaseAuth logOutAuth;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_nav_drawer_menu_fragment, container, false);
-        logOutAuth = FirebaseAuth.getInstance();
-        TextView textViewLogOut = view.findViewById(R.id.logOutID);
+
         TextView textViewSetting = view.findViewById(R.id.settingTextClick);
         TextView textViewMyLibrary = view.findViewById(R.id.myLib1);
         TextView textViewspeedTest = view.findViewById(R.id.speedTestID);
+        TextView textViewchooesBook = view.findViewById(R.id.chooes_book);
         LinearLayout linearLayout = view.findViewById(R.id.navDrawerMenu2);
 
         textViewSetting.setOnClickListener(new View.OnClickListener() {
@@ -69,6 +66,20 @@ FirebaseAuth logOutAuth;
             }
         });
 
+        textViewchooesBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getActivity().getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                choose_book_next_back_btns_fragment chooseBookNextBackBtnsFragment = new choose_book_next_back_btns_fragment();
+                choose_book_Q1 chooseBookQ1 = new choose_book_Q1();
+                ft.replace(android.R.id.content,chooseBookNextBackBtnsFragment);
+                ft.add(android.R.id.content,chooseBookQ1);
+                ft.commit();
+            }
+        });
+
+
 
 
         linearLayout.setOnClickListener(new View.OnClickListener() {
@@ -79,19 +90,10 @@ FirebaseAuth logOutAuth;
             }
         });
 
-        textViewLogOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                logOutAuth.signOut();
-                Intent intent = new Intent(getActivity(),Sign_In_fragment.class);
-                startActivity(intent);
-
-            }
-        });
-
 
         return view;
     }
+
 
     private void removeFragment() {
         if (requireActivity().getSupportFragmentManager() != null) {
@@ -101,14 +103,6 @@ FirebaseAuth logOutAuth;
                     .commit();
         }
 
-    }
 
 
-
-
-
-
-
-
-
-}
+    }}
