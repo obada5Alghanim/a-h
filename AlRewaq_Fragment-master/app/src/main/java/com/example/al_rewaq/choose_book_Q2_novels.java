3402e8fa1a7d575abd593284e06cae0e84611a3e
AsFoOr130 <mohalenezi130@gmail.com>
2024-05-03 19:47:45 +0300

    choose book for me not finashed

--- AlRewaq_Fragment-master/app/src/main/java/com/example/al_rewaq/choose_book_Q2_novels.java
+++ AlRewaq_Fragment-master/app/src/main/java/com/example/al_rewaq/choose_book_Q2_novels.java
@@ -9,13 +9,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-public class choose_book_Q2 extends Fragment {
+public class choose_book_Q2_novels extends Fragment {
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_choose_book__q2, container, false);
+        return inflater.inflate(R.layout.fragment_choose_book_q2_novels, container, false);
     }
 }
