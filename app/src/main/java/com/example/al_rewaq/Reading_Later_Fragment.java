d5771a2ec97063a46dd9b0e4a2a50f212ffac998
user <abada.alghanim55@gmail.com>
2024-05-29 22:39:04 +0300

    edit Reading_Later_Fragment

--- app/src/main/java/com/example/al_rewaq/Reading_Later_Fragment.java
+++ app/src/main/java/com/example/al_rewaq/Reading_Later_Fragment.java
@@ -10,6 +10,7 @@ import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -32,6 +33,7 @@ public class Reading_Later_Fragment extends Fragment {
     private FirebaseFirestore db;
     private FirebaseAuth auth;
     private String userId;
+    private RecyclerView.Adapter<RecyclerView.ViewHolder> adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -91,7 +93,7 @@ public class Reading_Later_Fragment extends Fragment {
     }
 
     private void updateRecyclerView() {
-        recyclerView.setAdapter(new RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        adapter = new RecyclerView.Adapter<RecyclerView.ViewHolder>() {
             @NonNull
             @Override
             public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -115,7 +117,7 @@ public class Reading_Later_Fragment extends Fragment {
                 });
 
                 deleteButton.setOnClickListener(v -> {
-                    removeBookFromReadingLater(position);
+                    removeBookFromReadingLater(holder.getAdapterPosition());
                 });
             }
 
@@ -123,7 +125,22 @@ public class Reading_Later_Fragment extends Fragment {
             public int getItemCount() {
                 return readingLaterBooks.size();
             }
+        };
+        recyclerView.setAdapter(adapter);
+
+        // Attach ItemTouchHelper to RecyclerView
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT | ItemTouchHelper.UP) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                removeBookFromReadingLater(viewHolder.getAdapterPosition());
+            }
         });
+        itemTouchHelper.attachToRecyclerView(recyclerView);
     }
 
     private void removeBookFromReadingLater(int position) {
@@ -132,7 +149,7 @@ public class Reading_Later_Fragment extends Fragment {
         userRef.update("Want_to_read", FieldValue.arrayRemove(book.getBookName()))
                 .addOnSuccessListener(aVoid -> {
                     readingLaterBooks.remove(position);
-                    updateRecyclerView();
+                    adapter.notifyItemRemoved(position);
                 })
                 .addOnFailureListener(e -> {
                     // Handle any errors here
